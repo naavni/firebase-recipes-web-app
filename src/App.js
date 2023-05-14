b133b22accec12d3c74aabd37d09547c269758fa
naavni <132861981+naavni@users.noreply.github.com>
2023-05-13 22:47:43 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import firebase from './FirebaseConfig';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="title-row">
+        <h1 className="title">Firebase Recipe</h1>
+        </div>
+    </div>
+  );
+}
+
+export default App;
