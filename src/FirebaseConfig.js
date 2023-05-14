84fd9a5cb3dbc8aa817921d82e09841f16c4a081
naavni <132861981+naavni@users.noreply.github.com>
2023-05-13 23:53:24 -0700

    silenced warning

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -3,13 +3,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: "AIzaSyA_XrZIXTKcfom6-Gny8E6MlXRA2wRvAXk",
+  authDomain: "fir-recipes-20c83.firebaseapp.com",
+  projectId: "fir-recipes-20c83",
+  storageBucket: "fir-recipes-20c83.appspot.com",
+  messagingSenderId: "968537816619",
+  appId: "1:968537816619:web:b63d6880d545073229f46e",
+  measurementId: "G-T7JCG8FC8B",
 };
 
 // Use this to initialize the firebase App
